030605ed168b7ac2ecf6436de4ac7beabad73171
hastichavda <hasiichavda10@gmail.com>
2019-08-17 18:32:25 +0530

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        {{-- <task-list></task-list> --}}
+        {{-- <task-list tasks="{{ $tasks }}" user="{{ $user }}"></task-list> --}}
+            <task-list :tasks="{{ json_encode($tasks) }}" :user="{{$user}}"></task-list>
+        </div>
+    </div>
+</div>
+@endsection
